9c825915466a79d85a05761e6abc5a5a8d002f80
Joshua Smith <mtgmirage@protonmail.com>
2025-03-06 13:38:20 -0600

    implemented applicationManager functions

--- src/managers/applicationManager.js
+++ src/managers/applicationManager.js
@@ -1,2 +1,17 @@
 const _apiUrl = "api/application";
 
+export const getApplicationsByJobId = (jobId) => {
+    return fetch(_apiUrl + "/" + jobId).then((res) => res.json());
+}
+
+export const createNewApplication = (jobId) => {
+    return fetch(_apiUrl + "/" + jobId, {
+        method: "POST"
+    })
+}
+
+export const cancelApplication = (jobId) => {
+    return fetch(_apiUrl + "/" + jobId, {
+        method: "DELETE"
+    })
+}
